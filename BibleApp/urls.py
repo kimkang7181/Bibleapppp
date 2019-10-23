2ae970a5f4c6ff0296ff5e981aed84d2110b1daf
kimkang7181 <kimkang7182@naver.com>
2019-10-23 16:26:21 +0900

    d

--- BibleApp/urls.py
+++ BibleApp/urls.py
@@ -1,26 +1,9 @@
-"""BibleApp URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from otrlarkdajtwoddl' rkddlrkchlrh
-    aptmfmf wnrdlslRk
-    roRNFE
-    dkdiffhfl
-     difffkffflff
-     hher_app.views import Home
-odiffhfl dff=)Ehbfbjhd2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from graphene_django.views import GraphQLView
+from BibleApp.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^graphql$', GraphQLView.as_view(graphiql=True, schema=schema)),
 ]

ee3ca6cc5188513a2c1b4f2bc43ee8558691813b
kimkang7181 <kimkang7182@naver.com>
2019-10-28 17:31:08 +0900

    app add

--- BibleApp/urls.py
+++ BibleApp/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, re_path
 from graphene_django.views import GraphQLView
 from BibleApp.schema import schema
+from django.conf import settings
+from django.conf.urls.static import static  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^graphql$', GraphQLView.as_view(graphiql=True, schema=schema)),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
